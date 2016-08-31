cf2a2972216635783e7066096a1838f5f0954ad8
Nirlendu Saha <nirlendu@Nirlendus-MacBook-Pro.local>
2016-08-31 23:11:32 +0530

    readme added and url bugfix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DjangoSampleApp
 Simple App in Django
+run Makefile
+Go to http://127.0.0.1:8000/
--- sampleApp/urls.py
+++ sampleApp/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+	url(r'^$', 'sites.views.index'),
 	url(r'^sites/$', 'sites.views.index'),
     url(r'^sites/1$', 'sites.views.demo_site'),
     url(r'^sites/2$', 'sites.views.abc_site'),
